014f1ae01889e33343a5b6adca52dd4981afb789
sitilyGITHUB <ggwp40913@gmail.com>
2023-03-21 22:38:06 +0600

    add info for any coins

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -14,7 +14,7 @@ const Cryptocurrencies = ({ simplified }) => {
   useEffect(() => {
     setCryptos(cryptosList?.data?.coins);
 
-    const filterData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    const filterData = cryptosList?.data?.coins.filter((item) => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
 
     setCryptos(filterData)
   }, [cryptosList, searchTerm])
@@ -32,8 +32,8 @@ const Cryptocurrencies = ({ simplified }) => {
     )}
       <Row gutter={[32, 32]} className='crypto-card-container'>
         {cryptos?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+          <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
+            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
               <Card 
               title={`${currency.rank}. ${currency.name}`}
               extra={<img className='crypto-image' src={currency.iconUrl}/>}
